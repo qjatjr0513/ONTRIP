da1e7583792bf0e02a7359ae138af0563e8b2736
SongJiho1998 <sung7054@naver.com>
2023-03-08 16:30:26 +0900

    03-08 vo
    03-08 vo

--- ONTRIP/src/main/java/com/kh/ontrip/question/vo/Question.java
+++ ONTRIP/src/main/java/com/kh/ontrip/question/vo/Question.java
@@ -1,11 +1,14 @@
-package com.ontrip.question.vo;
+package com.kh.ontrip.question.vo;
 
 import java.sql.Date;
 
+import lombok.Data;
+
+@Data
 public class Question {
 
     private Integer qCode;
-	// 성우  , Integer타입 변경
+	// �꽦�슦  , Integer���엯 蹂�寃�
 	private int memNo;
 	private String qTitle;
 	private String qContent;
@@ -13,98 +16,5 @@ public class Question {
 	private String aContent;
 	private Integer qCount;
 	private String status;
-
-    public Question() {
-    	
-    }
-
-	public Question(Integer qCode, int memNo, String qTitle, String qContent, Date qDate, String aContent, Integer qCount, String status) {
-		this.qCode = qCode;
-		this.memNo = memNo;
-		this.qTitle = qTitle;
-		this.qContent = qContent;
-		this.qDate = qDate;
-		this.aContent = aContent;
-		this.qCount = qCount;
-		this.status = status;
-	}
-
-
-	public Integer getqCode() {
-		return qCode;
-	}
-
-	public void setqCode(Integer qCode) {
-		this.qCode = qCode;
-	}
-
-	public int getMemNo() {
-		return memNo;
-	}
-
-	public void setMemNo(int memNo) {
-		this.memNo = memNo;
-	}
-
-	public String getqTitle() {
-		return qTitle;
-	}
-
-	public void setqTitle(String qTitle) {
-		this.qTitle = qTitle;
-	}
-
-	public String getqContent() {
-		return qContent;
-	}
-
-	public void setqContent(String qContent) {
-		this.qContent = qContent;
-	}
-
-	public Date getqDate() {
-		return qDate;
-	}
-
-	public void setqDate(Date qDate) {
-		this.qDate = qDate;
-	}
-
-	public String getaContent() {
-		return aContent;
-	}
-
-	public void setaContent(String aContent) {
-		this.aContent = aContent;
-	}
-
-	public Integer getqCount() {
-		return qCount;
-	}
-
-	public void setqCount(Integer qCount) {
-		this.qCount = qCount;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-	@Override
-	public String toString() {
-		return "Question{" +
-				"qCode=" + qCode +
-				", memNo=" + memNo +
-				", qTitle='" + qTitle + '\'' +
-				", qContent='" + qContent + '\'' +
-				", qDate=" + qDate +
-				", aContent='" + aContent + '\'' +
-				", qCount=" + qCount +
-				", status='" + status + '\'' +
-				'}';
-	}
+	
 }
