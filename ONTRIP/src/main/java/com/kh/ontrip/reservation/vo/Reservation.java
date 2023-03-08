ce450957ca5b93f577e9e22ee8f934694f554e52
SongJiho1998 <sung7054@naver.com>
2023-03-08 16:37:20 +0900

    03-08_vo -수정-
    03-08_vo -수정-

--- ONTRIP/src/main/java/com/kh/ontrip/reservation/vo/Reservation.java
+++ ONTRIP/src/main/java/com/kh/ontrip/reservation/vo/Reservation.java
@@ -7,25 +7,22 @@ import lombok.Data;
 @Data
 public class Reservation {
 	
-    private Integer rnCode; //�삁�빟 肄붾뱶
-    private int placeCode; // �떆�꽕 肄붾뱶
-    private int memberNo; // �쉶�썝 踰덊샇
-    private String rnName;; // �삁�빟�옄 紐�
-    private int rnAdult; // �씤�썝(�꽦�씤) �닔
-    private int rnChild; // �씤�썝(�뼱由곗씠) �닔
-    private int rnCount; // 媛앹떎 �닔
-    private String rnRoomType; // 媛앹떎���엯
-    private String rnPhoneNumber; // �삁�빟�옄 �쑕���룿 踰덊샇
-    private java.sql.Date rnCheckIn; // 泥댄겕�씤 �궇吏�
-    private java.sql.Date rnCheckOut; // 泥댄겕�븘�썐 �궇吏�
-    private java.sql.Date rnDate; // �삁�빟�븳 �궇吏�
-    private String rnStatus; // �궘�젣 �뿬遺�(N, Y)
+    private Integer rnCode;
+    private int placeCode; 
+    private int memberNo; 
+    private String rnName;; 
+    private int rnAdult;
+    private int rnChild; 
+    private int rnCount; 
+    private String rnRoomType;
+    private String rnPhoneNumber;
+    private Date rnCheckIn;
+    private Date rnCheckOut;
+    private Date rnDate;
+    private String rnStatus;
 	private Integer rnPrice;
 
 	private String plcName;
-
-
-	//寃곗젣�닔�떒 , 寃곗젣�젙蹂� , 移댄뀒怨좊━肄붾뱶  �궘�젣
 
 
 }
--- ONTRIP/src/main/java/com/kh/ontrip/score/vo/Score.java
+++ ONTRIP/src/main/java/com/kh/ontrip/score/vo/Score.java
@@ -6,11 +6,11 @@ import lombok.Data;
 public class Score {
 	
 	private int scoreCode;
-    private int reviewCode; // �썑湲곗퐫�뱶(PK)  // SQL : REV_CODE // VARCHAR2(20)
-    private double reviewStar;    // 留뚯”�룄        // SQL : REV_STAR // VARCHAR2(20)
-    private double review_c;      // 泥�寃�          // SQL : REV_C    // VARCHAR2(20)
-    private double review_s;      // �꽌鍮꾩뒪        // SQL : REV_S    // VARCHAR2(20)
-    private double review_p;      // �렪�쓽          // SQL : REV_P    // VARCHAR2(20)
+    private int reviewCode;
+    private double reviewStar; 
+    private double review_c;   
+    private double review_s;   
+    private double review_p;  
     private String memberId;
     private int memberNo;
     
